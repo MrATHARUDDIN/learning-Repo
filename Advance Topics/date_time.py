242001b8bc3b6100ec930ee57ffa941bdce1389f
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-24 20:17:31 +0200

    date and time module

--- Advance Topics/date_time.py
+++ Advance Topics/date_time.py
@@ -2,8 +2,22 @@ import datetime
 date = datetime.date(2025,1,2)
 today = datetime.date.today
 
+
+time = datetime.time(12,30,0) # hours -> munite -> second
+
+
 print(date)
 print(today)
 
 
+now = datetime.datetime.now()
+now = now.strftime("%H:%M:%S %d-%m-%Y")
+print("Current date : ",now)
 
+target_datetime = datetime.datetime(2020,1,2,12,30,1)
+current_time = datetime.datetime.now()
+
+if target_datetime < current_time:
+    print("Target has been passed")
+else:
+    print("You still have time")
