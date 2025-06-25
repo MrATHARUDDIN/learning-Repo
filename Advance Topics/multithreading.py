fe6b6a3dc15e4c3b992d61af8173f98b93a93de8
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-25 22:10:43 +0200

    Learning file

---
+++ Advance Topics/multithreading.py
@@ -0,0 +1,16 @@
+# Multithreading 🧵 is a concept in computer programming where a single process is divided into multiple threads 
+# that can run concurrently (at the same time). 
+# It’s commonly used to make programs faster and more efficient, especially when doing tasks that can happen in parallel.
+
+#      🧠 What is a Thread?
+#           A thread is the smallest unit of a process that can be scheduled by the operating system.
+
+
+# 🔧 Why use multithreading?
+# ✅ Improves performance on multi-core CPUs.
+
+# ✅ Better resource utilization.
+
+# ✅ Responsive user interfaces (UI doesn't freeze).
+
+# ✅ Faster execution for tasks like downloading files, real-time games, etc.
