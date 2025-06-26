b8473163822701bd3f10fd9da377e1fa8b7e267d
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-26 18:14:05 +0200

    api with py

--- Advance Topics/requestAPIdata.py
+++ Advance Topics/requestAPIdata.py
@@ -1,3 +1,12 @@
 # API stands for Application Programming Interface.
 # It allows two applications (or parts of code) to talk to each other.
 
+import requests
+
+# The API endpoint
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+response = requests.get(url)
+
+# print(response) it will print response [200] Means working
+print(response.json()) # now you can see the json data
