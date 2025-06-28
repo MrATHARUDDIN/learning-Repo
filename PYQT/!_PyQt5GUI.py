52521ec392b2e158c288dd24d8aa64ad4ed5ae03
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-28 10:34:06 +0200

    PyQt 5 first commit

--- PYQT/!_PyQt5GUI.py
+++ PYQT/!_PyQt5GUI.py
@@ -1,1 +1,7 @@
 # Gui means - Graphical User Interface
+import sys
+from PyQt5.QtWidgets import QApplication , QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
