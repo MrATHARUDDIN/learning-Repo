69d5885a780b30d5814b3736617e45c69595e333
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-28 10:41:22 +0200

    description of modules

--- PYQT/!_PyQt5GUI.py
+++ PYQT/!_PyQt5GUI.py
@@ -1,7 +1,17 @@
 # Gui means - Graphical User Interface
 import sys
+# sys --> System-level functions (like exiting the app)
+
 from PyQt5.QtWidgets import QApplication , QMainWindow
+# QApplication ---> Initializes the app and run the event loop
+
+# QMainWindow ---> Is a Main application window class 
+# It provides a framework with predefined areas like : menu bar, toolbars, status bar etc 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+
+def main():
+    pass
