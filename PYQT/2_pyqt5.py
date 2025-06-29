e31cdc51cbbe2184c5b64ce2943175b20520af9a
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-06-29 10:03:48 +0200

    new info

--- PYQT/2_pyqt5.py
+++ PYQT/2_pyqt5.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel,QPushButton
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap 
 from PyQt5.QtCore import Qt
+
+# QPixmap --> For the picture
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,10 +24,16 @@ class MainWindow(QMainWindow):
         label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
 
+        label2 =  QLabel(self)
+        label.setGeometry(0,0,500,100)
+
         btn = QPushButton("Check Out Girls",self) # QPushButton is the module of defining a btn
         btn.setCheckable(True) # set that as a clickable btn
         btn.setGeometry(50,150,90,30) # position of the btn
         btn.clicked.connect(self.after_click) # after click call the function
+
+    
+    
     # after click funtion    
     def after_click(self):
         print("Btn has been clicked")
