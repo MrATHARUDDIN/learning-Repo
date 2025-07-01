bea8042efaeffa3408025644e31be6ce5084f2ff
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-01 21:07:06 +0200

    Numpy 3d array

--- NumPy/1_Numpy.py
+++ NumPy/1_Numpy.py
@@ -2,6 +2,16 @@
 # NumPy is used for working with arrays.
 # NumPy is short for "Numerical Python".
 
+# Normal Array
 import numpy as np
 arr = np.array([1,2,3,4,5])
 print(arr)
+print()
+
+# 2D Array
+delo = np.array([[1, 2, 3], [4, 5, 6]])
+print(delo)
+
+
+# 3D Array
+third_arr = arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
