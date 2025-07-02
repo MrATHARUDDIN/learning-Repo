ffb6b7287c3e0b9a57abda0d6830d139d46ca7d4
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-02 18:25:28 +0200

    numpy type casting

--- NumPy/4_NumpyData.py
+++ NumPy/4_NumpyData.py
@@ -1,3 +1,6 @@
+
+
+
 # Below is a list of all data types in NumPy and the characters used to represent them.
 
 # i - integer
@@ -22,8 +25,28 @@ print(student.dtype)  # Output: <U6 (Unicode string of max length 6)
 
 
 
+a = np.array([1, 2, 3, 4], dtype='i4') # i4 means integer 4 bytes = 32bit
+#                                        This means each number in the array is stored using 4 bytes of memory
 
-a = np.array([1, 2, 3, 4], dtype='i4')
+print(a)         # Output: [1 2 3 4]
+print(a.dtype)   # Output: int32
 
-print(a)
-print(a.dtype)
+
+# Why Use Custom Data Types?
+
+# Save memory (e.g., use i1 if values are small)
+# Match external data (like from files or databases)
+# Optimize speed for large-scale numerical operations
+
+
+################## Converting data Type (type Casting in numpy)  ################## 
+# Converting data type means changing the type of elements in an existing array to another type—like converting an array of integers to floats, or strings to integers (if possible).
+
+#In NumPy, you do this using the .astype() method.
+
+
+c = np.array([1,2,3,4,5])
+print(c.dtype)
+
+floatarr = c.astype("float32") # so with astype we can swipe the value
+print(floatarr.dtype)
