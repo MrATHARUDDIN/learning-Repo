052e1c5a32ea156bbbac888da8345e2648daa760
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-04 23:55:58 +0200

    final push

--- NumPy/Important/filling_array.py
+++ NumPy/Important/filling_array.py
@@ -8,11 +8,13 @@ print(a)
 
 b = np.zeros((2,3,3)) # np.zeros((depth, rows, columns))
 print(b)  # Output: array of all zeros with shape (2, 3, 3)
- 
-
-
 
 # np.empty creates an array without filling it with zeros or ones
 c = np.empty((2,2,2))
 print(c) # np.empty creates an array without filling it with zeros or ones
 
+
+# Generate a 1D array of even numbers from 0 to 98 (step of 2)
+x_values = np.arange(0, 100, 2)  # np.arange(start, stop, step)
+print(x_values)
+# Output: [ 0  2  4  6 ... 96 98]
