06d27d184b4145de6e8be5770a4a141fa1b5cef8
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-05 10:04:55 +0200

    final edit

--- NumPy/Important/Math_operations.py
+++ NumPy/Important/Math_operations.py
@@ -10,3 +10,19 @@ print(a1 * 5) # with Numpy list
 
 # in python it just print the list 5 time
 # in numpy it will work as a matrix
+
+print(a1 + 5)
+print(a1 + a2)
+print(a1 * a2)
+
+print("------------------------")
+a1 = np.array([1,2,3])
+a2 = np.array([[1],[2]])
+
+print(a1 + a2)
+
+print("-----------Root elements -------------")
+
+a = np.array([[16,2,3],[4,5,100]])
+print(np.sqrt(a))
+# similarly we can use sin,cos,tan,arcsin etc method with our matrix
