b85879bbdfc24ee024d8f9b1d658c6a9bc369710
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-05 09:50:47 +0200

    math operation

---
+++ NumPy/Important/Math_operations.py
@@ -0,0 +1,12 @@
+import numpy as np
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,5]
+
+a1 = np.array(l1)
+a2 = np.array(l2)
+
+print(l1 * 5) # with python list
+print(a1 * 5) # with Numpy list
+
+# in python it just print the list 5 time
+# in numpy it will work as a matrix
