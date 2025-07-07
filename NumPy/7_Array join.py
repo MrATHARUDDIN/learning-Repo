06342dd05d12d1eebe22b9ee424449bcc9c37a46
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-07 16:24:38 +0200

    adding new info

--- NumPy/7_Array join.py
+++ NumPy/7_Array join.py
@@ -25,3 +25,8 @@ arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
 arr = np.stack((arr1, arr2), axis=1)         # create pairs column-wise
 print(arr)
+
+# vstack means with axis 0 (create pairs in row)
+# hstack means with axis 1 (create pairs in column)
+arr = np.vstack((arr1, arr2))
+arr = np.hstack((arr1, arr2))
